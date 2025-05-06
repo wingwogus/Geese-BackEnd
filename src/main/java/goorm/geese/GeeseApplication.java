c4363f70b5de92ed34a0d900e8e392f1e080c82c
이재현 <wingwogus@naver.com>
2025-05-07 00:27:17 +0900

    초기 설정

---
+++ src/main/java/goorm/geese/GeeseApplication.java
@@ -0,0 +1,13 @@
+package goorm.geese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goorm/geese/GeeseApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.geese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
